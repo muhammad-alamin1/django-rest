7b5fec464d7ab96b52840c7f888d51af4027cdac
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-06-20 01:06:39 +0600

    views update

--- bruass/views.py
+++ bruass/views.py
@@ -8,20 +8,20 @@ from django.http import HttpResponse
 #queryset
 class Information(View):
     # get all dataset
-    def get(self, request):
-        # complex data
-        teacher_complex_data = Bruass.objects.all()
-        # convert complex data to py dict
-        teacher_serializer_data = BruassSerializers(teacher_complex_data, many=True)
-        # render JSON
-        json_data = JSONRenderer().render(teacher_serializer_data.data)
-        
-        return HttpResponse(json_data, content_type='application/json')
+    def get(self, request, pk=None):
+        if pk:
+            complex_data = Bruass.objects.get(id=pk)
+            serializer = BruassSerializers(complex_data)
+            json_data = JSONRenderer().render(serializer.data)
+            
+            return HttpResponse(json_data, content_type='application/json')
+        else:
+            # complex data
+            teacher_complex_data = Bruass.objects.all()
+            # convert complex data to py dict
+            teacher_serializer_data = BruassSerializers(teacher_complex_data, many=True)
+            # render JSON
+            json_data = JSONRenderer().render(teacher_serializer_data.data)
+            
+            return HttpResponse(json_data, content_type='application/json')
     
-    # get single data
-    def get(self, request, pk):
-        complex_data = Bruass.objects.get(id=pk)
-        serializer = BruassSerializers(complex_data)
-        json_data = JSONRenderer().render(serializer.data)
-        
-        return HttpResponse(json_data, content_type='application/json')
